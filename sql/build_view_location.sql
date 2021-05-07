35974908e867eed6caca36d3db54a2ab4941eefe
Peter Dutey <p.dutey-magni@ucl.ac.uk>
2021-05-07 18:20:52 +0100

    update definitions
    - change NHNN ward designation
    - add T15 private wards
    - add T00 emergency assessment unit
    - change desination of P02-P03
    - add Elizabeth Garrett Anderson Wing
    - add Grafton Way Building
    - improve MCC identification (must be the start of HL7 block)

--- sql/build_view_location.sql
+++ sql/build_view_location.sql
@@ -16,55 +16,61 @@ CREATE VIEW flow.location AS
         ,location_string
         -- grab ward
         ,CASE
-            WHEN SPLIT_PART(location_string,'^',1) != 'null' then SPLIT_PART(location_string,'^',1)
-            WHEN SPLIT_PART(location_string,'^',1) = 'null' 
-                 AND SPLIT_PART(location_string,'^',2) ~ '^(T07CV.*)' then 'T07CV'
-            END AS ward
+		WHEN SPLIT_PART(location_string, '^',1) ~ '^(..NQ|HS15|HSDC)' then SPLIT_PART(SPLIT_PART(location_string, '^',2), ' ', 1)
+		WHEN SPLIT_PART(location_string, '^',2) ~ '^GWB' then SPLIT_PART(SPLIT_PART(location_string, '^',2), ' ', 2)
+		WHEN SPLIT_PART(location_string, '^',2) ~ '^(E02|E03)' then  SPLIT_PART(SPLIT_PART(location_string, '^',2), ' ', 1)
+		WHEN SPLIT_PART(location_string, '^',2) ~ '^UCH T00 EAU' then  'T00 EAU'
+		WHEN SPLIT_PART(location_string,'^',1) != 'null' then SPLIT_PART(location_string,'^',1)
+		WHEN SPLIT_PART(location_string,'^',1) = 'null' 
+			 AND SPLIT_PART(location_string,'^',2) ~ '^(T07CV.*)' then 'T07CV'
+  END AS ward
         ,SPLIT_PART(location_string,'^',3) bed
         ,SPLIT_PART(location_string,'^',1) ward_raw
-        -- define if census move or otherwise
-        ,CASE 
-            -- BEWARE / CHECK that this doesn't drop deaths or similar
-            -- DROP OPD etc where there is no bed
-            WHEN SPLIT_PART(location_string,'^',3) ~ '(BY|CB|CH|SR|HR|BD|NU|PD).*$|.*(MAJ|RAT|SDEC|RESUS|TRIAGE).*' then true 
-            WHEN SPLIT_PART(location_string,'^',3) IN ('null', 'POOL', 'WAIT', 'NONE', 'DISCHARGE', 'VIRTUAL') then false 
-            END AS census
-        -- define building / physical site
-        ,CASE 
-            -- THP3 includes podium theatres
-            WHEN SPLIT_PART(location_string,'^',1) ~ '^(T0|T1|THP3|ED(?!H))'  THEN 'tower'
-            WHEN SUBSTR(location_string,1,2) IN ('WM', 'WS')  THEN 'WMS'
-            WHEN location_string LIKE '%MCC%' then 'MCC'
-            WHEN location_string LIKE '%NICU%' then 'NICU'
-            WHEN location_string LIKE '%NHNN%' then 'NHNN'
-            WHEN SUBSTR(location_string,1,4) IN ('SINQ', 'MINQ')  THEN 'NHNN'
-            WHEN location_string LIKE 'EDH%' then 'EDH'
-            WHEN location_string LIKE '%OUTSC%' then 'EXTERNAL'
-            END AS building
-        -- define critical care
-        ,CASE 
-            WHEN SPLIT_PART(location_string,'^',1) ~ '^(T03|WSCC|SINQ|MINQ|P03CV|T07CV)' THEN true
-            WHEN SPLIT_PART(location_string,'^',2) ~ '^(T07CV.*)' THEN true
-            END AS critical_care
-        -- define ED areas
-        ,CASE 
-            WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%RESUS%' THEN 'RESUS'
-            WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%MAJ%' THEN 'MAJORS'
-            WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%UTC%' THEN 'UTC'
-            WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%RAT%' THEN 'RAT'
-            WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%SDEC%' THEN 'SDEC'
-            WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%SAA%' THEN 'SAA' -- specialty assessment area
-            WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%TRIAGE%' THEN 'TRIAGE'
-            WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%PAEDS%' THEN 'PAEDS'
-            END AS ed_zone
-        -- define bed type
-        ,CASE
-            WHEN SUBSTR(SPLIT_PART(location_string,'^',3),1,2) IN ('BY', 'CB') then 'bay' 
-            WHEN SUBSTR(SPLIT_PART(location_string,'^',3),1,2) IN ('SR') then 'sideroom' 
-            WHEN SUBSTR(SPLIT_PART(location_string,'^',3),1,2) IN ('CH') then 'chair' 
-            WHEN location_string ~ '.*(SURGERY|THR|PROC|ENDO|TREAT|ANGI).*|.+(?<!CHA)IN?R\^.*' then 'procedure' 
-            WHEN location_string ~ '.*XR.*|.*MRI.*|.+CT\^.*|.*SCANNER.*' then 'imaging' 
-            END AS bed_type
+	-- define if census move or otherwise
+	,CASE 
+		-- BEWARE / CHECK that this doesn't drop deaths or similar
+		-- DROP OPD etc where there is no bed
+		WHEN SPLIT_PART(location_string,'^',3) ~ '(BY|CB|CH|SR|HR|BD|NU|PD).*$|.*(MAJ|RAT|SDEC|RESUS|TRIAGE).*' then true 
+		WHEN SPLIT_PART(location_string,'^',3) IN ('null', 'POOL', 'WAIT', 'NONE', 'DISCHARGE', 'VIRTUAL') then false 
+		END AS census
+	-- define building / physical site
+	,CASE 
+		-- THP3 includes podium theatres
+		    WHEN SPLIT_PART(location_string,'^',1) ~ '^(T0|T1|THP3|ED(?!H)|P03CV|P02IR|P02G|EAU|HS15|HSDC)'  THEN 'tower'
+            WHEN SPLIT_PART(location_string, '^',1) ~ '^..NQ' then 'NHNN'
+		    WHEN SUBSTR(location_string,1,2) IN ('WM', 'WS')  THEN 'WMS'
+            WHEN SPLIT_PART(location_string, '^',1) ~ '^MCC' or SPLIT_PART(location_string, '^',2) ~ '^MCC' then 'MCC'
+		    WHEN location_string LIKE '%NICU%' then 'NICU'
+		    WHEN location_string LIKE '%NHNN%' then 'NHNN'
+		    WHEN location_string LIKE 'EDH%' then 'EDH'
+		    WHEN location_string LIKE '%OUTSC%' then 'EXTERNAL'
+            WHEN SUBSTR(location_string, 1, 10) IN ('1021800003', '1021800004') then 'GWB'
+            WHEN SPLIT_PART(location_string, '^',2) ~ '^(E02|E03)' then 'EGA'
+		END AS building
+	-- define critical care
+	,CASE 
+		WHEN SPLIT_PART(location_string,'^',1) ~ '^(T03|WSCC|SINQ|MINQ|P03CV|T07CV)' THEN true
+		WHEN SPLIT_PART(location_string,'^',2) ~ '^(T07CV.*)' THEN true
+		END AS critical_care
+	-- define ED areas
+	,CASE 
+		WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%RESUS%' THEN 'RESUS'
+		WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%MAJ%' THEN 'MAJORS'
+		WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%UTC%' THEN 'UTC'
+		WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%RAT%' THEN 'RAT'
+		WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%SDEC%' THEN 'SDEC'
+		WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%SAA%' THEN 'SAA' -- specialty assessment area
+		WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%TRIAGE%' THEN 'TRIAGE'
+		WHEN SPLIT_PART(location_string,'^',1) = 'ED' AND location_string LIKE '%PAEDS%' THEN 'PAEDS'
+		END AS ed_zone
+	-- define bed type
+	,CASE
+		WHEN SUBSTR(SPLIT_PART(location_string,'^',3),1,2) IN ('BY', 'CB') then 'bay' 
+		WHEN SUBSTR(SPLIT_PART(location_string,'^',3),1,2) IN ('SR') then 'sideroom' 
+		WHEN SUBSTR(SPLIT_PART(location_string,'^',3),1,2) IN ('CH') then 'chair' 
+		WHEN location_string ~ '.*(SURGERY|THR|PROC|ENDO|TREAT|ANGI).*|.+(?<!CHA)IN?R\^.*' then 'procedure' 
+		WHEN location_string ~ '.*XR.*|.*MRI.*|.+CT\^.*|.*SCANNER.*' then 'imaging' 
+		END AS bed_type
     FROM star_a.location
 ORDER BY location_string ASC
 ;
